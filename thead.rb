ce87be02088c3093dfd6d67518413bfe83225abf
akanatr <tana@tanaka.local>
2021-07-12 15:53:05 +0900

    upadate

---
+++ thead.rb
@@ -0,0 +1,12 @@
+HOMEBREW_THEAD_VERSION="1.0.0"
+
+class Thead < Formula
+  desc "Eliminating duplicated lines"
+  homepage "https://github.com/akanatr/thead"
+  url "https://github.com/akanatr/thead/releases/download/v#{HOMEBREW_THEAD_VERSION}/thead-#{HOMEBREW_THEAD_VERSION}_darwin_amd64.tar.gz"
+  version HOMEBREW_THEAD_VERSION
+
+  def install
+    bin.install "thead"
+  end
+end
