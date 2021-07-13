684bd0e22b5d74d43a4ce660a31eb52404550db8
akanatr <tana@tanaka.local>
2021-07-13 10:59:48 +0900

    update

--- thead.rb
+++ thead.rb
@@ -3,7 +3,7 @@ HOMEBREW_THEAD_VERSION="1.0.0"
 class Thead < Formula
   desc "Eliminating duplicated lines"
   homepage "https://github.com/akanatr/thead"
-  url "https://github.com/akanatr/thead/releases/download/v#{HOMEBREW_THEAD_VERSION}/thead-#{HOMEBREW_THEAD_VERSION}_darwin_amd64.tar.gz"
+  url "https://github.com/akanatr/thead/releases/download/#{HOMEBREW_THEAD_VERSION}/thead-#{HOMEBREW_THEAD_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_THEAD_VERSION
 
   def install
