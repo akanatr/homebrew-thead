0e596fa1b4c396c783ff055d2b92de99b822be9e
akanatr <tana@tanaka.local>
2021-07-27 09:24:37 +0900

    update

--- thead.rb
+++ thead.rb
@@ -6,7 +6,7 @@ class Thead < Formula
   url "https://github.com/akanatr/thead/releases/download/#{HOMEBREW_THEAD_VERSION}/thead-#{HOMEBREW_THEAD_VERSION}_darwin_amd64.tar.gz"
   version HOMEBREW_THEAD_VERSION
 
-  sha256 "d24ad2591af633ca5602231897ab3fde93167ee85b90a45bbc78af7f79fd8965"
+  sha256 "a365c0d4cb726fd88b56c46af1271855131ede419ff2b9c735519188ec8d1062"
 
   def install
     bin.install "thead"
